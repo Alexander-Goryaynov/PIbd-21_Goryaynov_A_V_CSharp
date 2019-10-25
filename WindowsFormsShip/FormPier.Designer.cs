b60ab42ebeeda14c09e4d0dc6768c0469e585e9e
Alexander-Goryaynov <gor.alex595@gmail.com>
2019-10-25 09:32:13 +0400

    lab4done

--- WindowsFormsShip/FormPier.Designer.cs
+++ WindowsFormsShip/FormPier.Designer.cs
@@ -32,11 +32,13 @@
             this.btnParkShip = new System.Windows.Forms.Button();
             this.btnParkDieselShip = new System.Windows.Forms.Button();
             this.groupBox = new System.Windows.Forms.GroupBox();
-            this.label = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
-            this.buttonTake = new System.Windows.Forms.Button();
             this.pictureBoxTake = new System.Windows.Forms.PictureBox();
+            this.buttonTake = new System.Windows.Forms.Button();
+            this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.labelLevels = new System.Windows.Forms.Label();
+            this.listBoxLevels = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPier)).BeginInit();
             this.groupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTake)).BeginInit();
@@ -52,9 +54,9 @@
             // 
             // btnParkShip
             // 
-            this.btnParkShip.Location = new System.Drawing.Point(928, 12);
+            this.btnParkShip.Location = new System.Drawing.Point(903, 12);
             this.btnParkShip.Name = "btnParkShip";
-            this.btnParkShip.Size = new System.Drawing.Size(103, 60);
+            this.btnParkShip.Size = new System.Drawing.Size(88, 45);
             this.btnParkShip.TabIndex = 1;
             this.btnParkShip.Text = "Поставить корабль";
             this.btnParkShip.UseVisualStyleBackColor = true;
@@ -62,9 +64,9 @@
             // 
             // btnParkDieselShip
             // 
-            this.btnParkDieselShip.Location = new System.Drawing.Point(928, 90);
+            this.btnParkDieselShip.Location = new System.Drawing.Point(1007, 12);
             this.btnParkDieselShip.Name = "btnParkDieselShip";
-            this.btnParkDieselShip.Size = new System.Drawing.Size(103, 62);
+            this.btnParkDieselShip.Size = new System.Drawing.Size(91, 45);
             this.btnParkDieselShip.TabIndex = 2;
             this.btnParkDieselShip.Text = "Поставить теплоход";
             this.btnParkDieselShip.UseVisualStyleBackColor = true;
@@ -83,30 +85,13 @@
             this.groupBox.TabIndex = 3;
             this.groupBox.TabStop = false;
             // 
-            // label
+            // pictureBoxTake
             // 
-            this.label.AutoSize = true;
-            this.label.Location = new System.Drawing.Point(22, 16);
-            this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(94, 13);
-            this.label.TabIndex = 0;
-            this.label.Text = "Забрать корабль";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 38);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Место: ";
-            // 
-            // maskedTextBox
-            // 
-            this.maskedTextBox.Location = new System.Drawing.Point(73, 35);
-            this.maskedTextBox.Name = "maskedTextBox";
-            this.maskedTextBox.Size = new System.Drawing.Size(43, 20);
-            this.maskedTextBox.TabIndex = 2;
+            this.pictureBoxTake.Location = new System.Drawing.Point(6, 110);
+            this.pictureBoxTake.Name = "pictureBoxTake";
+            this.pictureBoxTake.Size = new System.Drawing.Size(189, 148);
+            this.pictureBoxTake.TabIndex = 4;
+            this.pictureBoxTake.TabStop = false;
             // 
             // buttonTake
             // 
@@ -118,19 +103,56 @@
             this.buttonTake.UseVisualStyleBackColor = true;
             this.buttonTake.Click += new System.EventHandler(this.buttonTake_Click);
             // 
-            // pictureBoxTake
+            // maskedTextBox
             // 
-            this.pictureBoxTake.Location = new System.Drawing.Point(6, 110);
-            this.pictureBoxTake.Name = "pictureBoxTake";
-            this.pictureBoxTake.Size = new System.Drawing.Size(189, 148);
-            this.pictureBoxTake.TabIndex = 4;
-            this.pictureBoxTake.TabStop = false;
+            this.maskedTextBox.Location = new System.Drawing.Point(73, 35);
+            this.maskedTextBox.Name = "maskedTextBox";
+            this.maskedTextBox.Size = new System.Drawing.Size(43, 20);
+            this.maskedTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Место: ";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(22, 16);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(94, 13);
+            this.label.TabIndex = 0;
+            this.label.Text = "Забрать корабль";
+            // 
+            // labelLevels
+            // 
+            this.labelLevels.AutoSize = true;
+            this.labelLevels.Location = new System.Drawing.Point(912, 70);
+            this.labelLevels.Name = "labelLevels";
+            this.labelLevels.Size = new System.Drawing.Size(48, 13);
+            this.labelLevels.TabIndex = 4;
+            this.labelLevels.Text = "Уровни:";
+            // 
+            // listBoxLevels
+            // 
+            this.listBoxLevels.FormattingEnabled = true;
+            this.listBoxLevels.Location = new System.Drawing.Point(911, 94);
+            this.listBoxLevels.Name = "listBoxLevels";
+            this.listBoxLevels.Size = new System.Drawing.Size(177, 160);
+            this.listBoxLevels.TabIndex = 5;
+            this.listBoxLevels.SelectedIndexChanged += new System.EventHandler(this.listBoxLevels_SelectedIndexChanged);
             // 
             // FormPier
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1115, 585);
+            this.Controls.Add(this.listBoxLevels);
+            this.Controls.Add(this.labelLevels);
             this.Controls.Add(this.groupBox);
             this.Controls.Add(this.btnParkDieselShip);
             this.Controls.Add(this.btnParkShip);
@@ -143,6 +165,7 @@
             this.groupBox.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTake)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -157,5 +180,7 @@
         private System.Windows.Forms.MaskedTextBox maskedTextBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label labelLevels;
+        private System.Windows.Forms.ListBox listBoxLevels;
     }
 }
--- WindowsFormsShip/Pier.cs
+++ WindowsFormsShip/Pier.cs
@@ -9,28 +9,27 @@ namespace WindowsFormsShip
 {
     class Pier<T> where T : class, ITransport
     {
-        private T[] places;
+        private Dictionary<int, T> places;
+        private int maxCount;
         private int PictureWidth { get; set; }
         private int PictureHeight { get; set; }
         private const int placeSizeWidth = 210;
         private const int placeSizeHeight = 80;
         public Pier(int sizes, int pictureWidth, int pictureHeight)
         {
-            places = new T[sizes];
+            maxCount = sizes;
+            places = new Dictionary<int, T>();
             PictureWidth = pictureWidth;
             PictureHeight = pictureHeight;
-            for (int i = 0; i < places.Length; i++)
-            {
-                places[i] = null;
-            }
         }
         public static int operator +(Pier<T> p, T ship)
         {
-            for (int i = 0; i < p.places.Length; i++)
+            if (p.places.Count == p.maxCount) return -1;
+            for (int i = 0; i < p.maxCount; i++)
             {
                 if (p.CheckFreePlace(i))
                 {
-                    p.places[i] = ship;
+                    p.places.Add(i, ship);
                     p.places[i].SetPosition(5 + i / 5 * placeSizeWidth + 50, 
                         i % 5 * placeSizeHeight + 45, p.PictureWidth, p.PictureHeight);
                     return i;
@@ -40,38 +39,32 @@ namespace WindowsFormsShip
         }
         public static T operator -(Pier<T> p, int index)
         {
-            if (index < 0 || index > p.places.Length)
-            {
-                return null;
-            }
             if (!p.CheckFreePlace(index))
             {
                 T ship = p.places[index];
-                p.places[index] = null;
+                p.places.Remove(index);
                 return ship;
             }
             return null;
         }
         private bool CheckFreePlace(int index)
         {
-            return places[index] == null;
+            return !(places.ContainsKey(index));
         }
         public void Draw(Graphics g)
         {
             DrawMarking(g);
-            for (int i = 0; i < places.Length; i++)
+            var keys = places.Keys.ToList();
+            for (int i = 0; i < keys.Count; i++)
             {
-                if (!CheckFreePlace(i))
-                {
-                    places[i].DrawShip(g);
-                }
+                places[keys[i]].DrawShip(g);
             }
         }
         private void DrawMarking(Graphics g)
         {
             Pen pen = new Pen(Color.Black, 3);
-            g.DrawRectangle(pen, 0, 0, (places.Length / 5) * placeSizeWidth, 480);
-            for (int i = 0; i < places.Length / 5; i++)
+            g.DrawRectangle(pen, 0, 0, (maxCount / 5) * placeSizeWidth, 480);
+            for (int i = 0; i < maxCount / 5; i++)
             {
                 for (int j = 0; j < 6; j++)
                 {
